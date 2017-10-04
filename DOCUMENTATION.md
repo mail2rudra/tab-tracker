5f3547a5f530c5cb6c569857ebff918ecf2c43d8
mail2rudra33 <mail2rudra33@gmail.com>
2017-10-04 22:43:05 +0545

    setting up the registeration page

--- DOCUMENTATION.md
+++ DOCUMENTATION.md
@@ -8,3 +8,4 @@
 # npm install --save nodemon eslint
 # for Eslint init run this command node ./node_modules/eslint/bin/eslint.js  --init
 # npm install --save express body-parser cors morgan
+# npm install --save axios
---
+++ client/src/components/Register.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <input type="email" name="email" v-model="email" placeholder="Email"><br><br>
+    <input type="password" name="password" v-model="password" placeholder="Password"><br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      msg: 'Register',
+      email: '',
+      password: ''
+    }
+  },
+  watch: {
+    // email (value) {
+    //   console.log('email has changed', value)
+    // },
+    // password (value) {
+    //   console.log('Password has changed', value)
+    // }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+      // console.log('register button was clicked', this.email, this.password)
+    }
+  },
+  mounted () {
+    // setTimeout(() => {
+    //   this.email = 'Hello World!'
+    // }, 2000)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
